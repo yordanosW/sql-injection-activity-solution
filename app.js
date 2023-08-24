9ef98172907e36d4ba0dcbd1bc7bec964d4339a7
christopher-distasio <stahzz1970@gmail.com>
2023-08-23 19:21:46 -0500

    Adds JS and db starter code

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require('http'),
+  path = require('path'),
+  express = require('express'),
+  bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
+const app = express();
+app.use(express.static('.'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+const db = new sqlite3.Database(':memory:');
+db.serialize(function () {
+ db.run("CREATE TABLE user (username TEXT, password TEXT, title TEXT)");
+ db.run("INSERT INTO user VALUES ('privilegedUser', 'privilegedUser1', 'Administrator')");
+});
