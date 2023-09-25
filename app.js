77e5d9453ca3b5735e7cc06586d1fcf60f719cff
christopher-distasio <stahzz1970@gmail.com>
2023-09-24 19:37:26 -0500

    Updates instructions, code

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+//Activity Companion for SQL-Injection-Demo
+
+//After finishing the Project Setup, Starter Code: HTML, and Starter Code: Javascript sections, you can pick up from there with this code:   
+
+//Note: the suggested answer in the first box under the Mount the Attack section is incorrect; the answer should be "We SHOULDN'T have been logged in!"
+
+
 const sqlite3 = require('sqlite3').verbose();
 const http = require('http'),
-	path = require('path'),
-	express = require('express'),
-	bodyParser = require('body-parser');
+path = require('path'),
+express = require('express'),
+bodyParser = require('body-parser');
 
 
 const app = express();
@@ -10,16 +17,14 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-
-
-
 const db = new sqlite3.Database(':memory:');
 db.serialize(function () {
 	db.run("CREATE TABLE user (username TEXT, password TEXT, title TEXT)");
-	db.run("INSERT INTO user VALUES ('ss', 'ss', 'ss')");
+	
+	//Make sure you really do change the user values if you're working from the HackerU source code.
+
+	db.run("INSERT INTO user VALUES ('aa', 'aa', 'User of an app with vulnerable source code')");
 });
-
-
 
 
 app.get('/', function (req, res) {
@@ -27,36 +32,33 @@ app.get('/', function (req, res) {
 });
 
 
-
-
 app.post('/login', function (req, res) {
 	const username = req.body.username;
 	const password = req.body.password;
 
 
-	// Your goal as a white hat hacker is to send a malicious query that will trick the user into thinking they are really logged in when they aren't, all
-	// the while "injecting" SQL statements to the database to manipulate it for evil doings. 
-
-
 	// This is the legitimate query that is run in the backend (how depends on the specific programming language ecosystem and framework being used)
-	// when the user enters their username and password in the form, as is normally done with apps using a SQL DBMS:
-
-
+	// when the user enters their username and password in the form:
+	
 	const query = "SELECT title FROM user where username = '" + username + "' and password = '" + password + "'";
 
 
-	// Keep in mind that allowing a string to be passed directly into your database like this as a parameter in a route handler,
-	// especially if it's not set up as a prepared statement first, is NOT a good way to make SQL queries for production because it makes your app extremely vulnerable.
-	// It's only for coding in development mode, your own non-commercial portfolio projects, or for a demo like this.
+	// Your goal as a white hat hacker (and to satisfy the Acceptance Criteria in the graded assignment) is to send a query that will allow a would-be attacker to log in
+	// as the actual user legitimately would (with even more malicious activity possible if further clauses were added to this string of input suggested in the activity:  unknown' OR '1'='1   
 
-	// This is the exact input that will achieve the result when entered in the username field and the correct password is used: unknown' OR '1'='1
+
+	console.log("username: " + username);
+	console.log("password: " + password);
+	console.log('query: ' + query);
+
+
+	// Keep in mind that allowing a string to be passed directly into your database like this as a parameter in a route handler, especially if it's not set up as a sanitizing prepared statement first, 
+	// is NOT a good way to make SQL queries for production because it makes your app extremely vulnerable. It's only for coding in development mode, your own non-commercial portfolio projects, or for a demo like this. 
 	
-	// When the user has a successful login even though they've entered these malicious characters rather than the correct username, you'll know your hack has been a success!
+	// This is the exact input that will achieve the result when entered in the username field and the correct password is used: unknown' OR '1'='1
 
+	// When the user has a successful login even though they've entered these malicious characters rather than their correct username, you'll know your hack has been a success!
 
-	// console.log("username: " + username);
-	// console.log("password: " + password);
-	// console.log('query: ' + query);
 
 	db.get(query, function (err, row) {
 		if (err) {
@@ -66,19 +68,17 @@ app.post('/login', function (req, res) {
 		} else if (!row) {
 			res.redirect("/index.html#unauthorized");
 		} else {
-			res.send('Login successful');
-
+			res.send(
+			"Hello <b>" + row.title + '!</b><br /> This file contains all your secret data: <br /><br /> SECRETS <br /><br /> MORE SECRETS <br /><br /> <a href="/index.html">Go back to login</a>'
+			)
+			//res.send('Login successful')
 
 			// NOTES:
-			// 1. To fully experience how SQL Injection works with this awesome Graded Activity, you need to replace the long malicious attack code (shown below) with 'Login successful'(as shown on line 69 above).
-			// so that you can see the user log in even with the incorrect input (since as the hacker you'll have entered unknown' OR '1'='1 to freak out the SQL mechanism).
+			// 1.When you're finished with the commenting out the long "malicious attack code" in the db.get(query...) method call above. This code was just some hardcoded fun.
 
+			// 2. Then uncomment out res.send('Login successful') so that you can see the user log in successfully.
 
-			// This is the code from the activity you're replacing (on line 69):
-			// "Hello <b>" +; row.title + '!</b><br /> This file contains all your secret data: <br /><br /> SECRETS <br /><br /> MORE SECRETS <br /><br /> <a href="/index.html">Go back to login</a>'
-
-
-			// 2. If you run into any weird errors that pop up even if you've had the app run correctly before, try deleting the package-lock.json file and the node_modules folder then run npm install again
+			// 3. If you run into any weird errors that pop up even if you've had the app run correctly before, try deleting the package-lock.json file and the node_modules folder then run npm install again.
 
 
 		}
